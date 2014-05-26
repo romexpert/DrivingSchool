d5b8412290a352fc69e230a54cb2973dc8e3be67
admin <admin@172.16.23.211>
2014-05-26 14:22:39 +0400

    Lambda filtering example

--- src/java/StaffApi.java
+++ src/java/StaffApi.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,15 +23,18 @@ public class StaffApi extends HttpServlet {
         
         //TODO
         //Handle "role" filter
-        
+        String roleParam = request.getParameter("role");
+        AccountRole role = roleParam != null ? AccountRole.valueOf(roleParam) : null;
         ArrayList<Person> staff = new ArrayList<>();
         staff.add(new Person(1, "Админ Андрюша", "admin@driftman.ru", "1 2345678", AccountRole.Admin));
         staff.add(new Person(2, "Преподаватель 2", "teacher@driftman.ru", "2 2345678", AccountRole.Teacher));
         staff.add(new Person(3, "Преподаватель 3", "teacher1@driftman.ru", "3 2345678", AccountRole.Teacher));
         staff.add(new Person(1, "Преподаватель 4", "teacher2@driftman.ru", "4 2345678", AccountRole.Teacher));
         
+        List<Person> result = staff.stream().filter(person -> role == null || person.getRole() == role).collect(Collectors.toList());
+        
         JSONArray data = new JSONArray();
-        data.addAll(staff);
+        data.addAll(result);
 
         data.writeJSONString(response.getWriter());
     }
