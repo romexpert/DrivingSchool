7559359c83c93ff078f9412895ee5adfcec05321
admin <admin@172.16.23.211>
2014-05-26 19:14:26 +0400

    Filtering with flags

--- src/java/StaffApi.java
+++ src/java/StaffApi.java
@@ -14,6 +14,8 @@ import org.json.simple.JSONArray;
  */
 public class StaffApi extends HttpServlet {
 
+    private static final EnumSet<AccountRole> _staffRoles = EnumSet.of(AccountRole.Admin, AccountRole.Teacher);
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -29,9 +31,13 @@ public class StaffApi extends HttpServlet {
         staff.add(new Person(1, "Админ Андрюша", "admin@driftman.ru", "1 2345678", AccountRole.Admin));
         staff.add(new Person(2, "Преподаватель 2", "teacher@driftman.ru", "2 2345678", AccountRole.Teacher));
         staff.add(new Person(3, "Преподаватель 3", "teacher1@driftman.ru", "3 2345678", AccountRole.Teacher));
-        staff.add(new Person(1, "Преподаватель 4", "teacher2@driftman.ru", "4 2345678", AccountRole.Teacher));
+        staff.add(new Person(4, "Преподаватель 4", "teacher2@driftman.ru", "4 2345678", AccountRole.Teacher));
         
-        List<Person> result = staff.stream().filter(person -> role == null || person.getRole() == role).collect(Collectors.toList());
+        staff.add(new Person(5, "Студент 5", "student@driftman.ru", "5 2345678", AccountRole.Student));
+        staff.add(new Person(6, "Студент 6", "student2@driftman.ru", "6 2345678", AccountRole.Student));
+        staff.add(new Person(7, "Студент 7", "student3@driftman.ru", "7 2345678", AccountRole.Student));
+        
+        List<Person> result = staff.stream().filter(person -> (role == null && _staffRoles.contains(person.getRole())) || person.getRole() == role).collect(Collectors.toList());
         
         JSONArray data = new JSONArray();
         data.addAll(result);
